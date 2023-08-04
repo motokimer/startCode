d298ccb7abce1c5a19dff3c9395144477c945075
MotokiMera <motokimera@yahoo.co.jp>
2023-08-04 15:38:13 +0900

    test

---
+++ search.py
@@ -0,0 +1,15 @@
+import csv
+
+search_name = input("検索する名前を入力してください: ")
+
+with open('sample.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    found = False
+    for row in reader:
+        if row['name'] == search_name:
+            print(f"{search_name} は {row['city']} に住んでいます。")
+            found = True
+            break
+    
+    if not found:
+        print(f"{search_name} は見つかりませんでした。")
